91273395d870ba1ca6ebb4a5dcf837920d649670
patrickbui01 <bui.patrick01@gmail.com>
2025-03-22 13:37:49 +0100

    Update justA.py

--- justA.py
+++ justA.py
@@ -1,6 +1,5 @@
 # This is a simple Python script that prints "Hello, World!" to the console
-def hello_world():
-    return "Hello, World!"
 
-if __name__ == "__main__":
-    print(hello_world())
+from huggingface_hub import notebook_login
+
+notebook_login()
